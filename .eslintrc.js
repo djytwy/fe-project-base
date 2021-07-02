8725caa33bfb8d7347443650e04fc8f98a880ace
djytwy <676534074@qq.com>
2021-07-02 15:21:08 +0800

    update and add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,36 @@
 module.exports = {
-  'extends': ['alloy', 'alloy/react', 'alloy/typescript'],
-  'env': {
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  plugins: ['prettier'],
+  env: {
     // Your environments (which contains several predefined global variables)
     //
-    'browser': true,
-    'node': true,
-    'es6': true,
+    browser: true,
+    node: true,
+    es6: true
     // mocha: true,
     // jest: true,
     // jquery: true
   },
-  'globals': {
+  globals: {
     // Your global variables (setting to false means it"s not allowed to be reassigned)
-    //
     // myGlobal: false
   },
-  'rules': {
+  rules: {
     // Customize your rules
+    'prettier/prettier': 'warn',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     // js/ts
-    'eol-last': 'error',  // 禁止文件末尾保留一行空行
+    'eol-last': 'error', // 禁止文件末尾保留一行空行
     'no-trailing-spaces': 'error', // 禁用行尾空白
     // 'comma-style': ['error', 'last'],  // 逗号的风格，在末尾补上逗号
-    'comma-dangle': ['error', 'always-multiline'],  // 在[].{}中换行的尾部用上逗号
-    'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }], // 默认单引号
-    'camelcase': ['error', { properties: 'never' }], // 驼峰语法
-    'semi': ['error', 'never'], // 禁止使用半分号
-    'indent': ['error', 2, { SwitchCase: 1 }],  // 前空隙，2个单位
+    // 'comma-dangle': ['error', 'always-multiline'], // 在[].{}中换行的尾部用上逗号
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }], // 默认单引号
+    camelcase: ['error', { properties: 'never' }], // 驼峰语法
+    // semi: ['error', 'never'], // 禁止使用半分号
+    indent: ['error', 2, { SwitchCase: 1 }], // 前空隙，2个单位
     'object-curly-spacing': ['error', 'always'], // 强制在{}中使用一致的空格
-    'arrow-parens': ['error', 'as-needed'], // 箭头函数需要括号
+    // 'arrow-parens': ['error', 'as-needed'], // 箭头函数需要括号
     '@typescript-eslint/explicit-module-boundary-types': 'off', // ts的约束
-    '@typescript-eslint/no-explicit-any': 'off', // ts的约束
-    '@typescript-eslint/member-delimiter-style': [  // ts的约束
-      'error',
-      {
-        multiline: {
-          delimiter: 'none',
-          requireLast: false,
-        },
-        singleline: {
-          delimiter: 'semi',
-          requireLast: true,
-        },
-      },
-    ],
-  },
-}
+    '@typescript-eslint/no-explicit-any': 'off' // ts的约束
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,36 @@
-import { ConfigEnv, UserConfigExport, Plugin } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import legacy from '@vitejs/plugin-legacy'
-import vitePluginImp from 'vite-plugin-imp'
-import visualizer from 'rollup-plugin-visualizer'
-import path from 'path'
-import fs from 'fs'
-import dotenv from 'dotenv'
-import { minifyHtml } from 'vite-plugin-html'
-import compress from 'vite-plugin-compress'
+import { ConfigEnv, UserConfigExport, Plugin } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import legacy from '@vitejs/plugin-legacy';
+import vitePluginImp from 'vite-plugin-imp';
+import visualizer from 'rollup-plugin-visualizer';
+import path from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { minifyHtml } from 'vite-plugin-html';
+import compress from 'vite-plugin-compress';
 
 const config: UserConfigExport = {
   plugins: [
     compress(),
     reactRefresh(),
     legacy({
-      targets: [
-        'Android >= 39',
-        'Chrome >= 50',
-        'Safari >= 10.1',
-        'iOS >= 10.3',
-        '> 1%',
-        'not IE 11',
-      ],
+      targets: ['Android >= 39', 'Chrome >= 50', 'Safari >= 10.1', 'iOS >= 10.3', '> 1%', 'not IE 11']
     }),
     // antd 按需引入
     vitePluginImp({
       libList: [
         {
           libName: 'antd',
-          style: name => `antd/es/${name}/style`,
-          libDirectory: 'es',
-        },
-      ],
-    }),
+          style: (name) => `antd/es/${name}/style`,
+          libDirectory: 'es'
+        }
+      ]
+    })
   ],
   resolve: {
-    alias: [
-      {
-        find: /@\//,
-        replacement: path.join(__dirname, './src/'),
-      },
-    ],
+    alias: {
+      '@/': path.join(__dirname, './src/')
+    }
   },
   css: {
     preprocessorOptions: {
@@ -49,13 +39,13 @@ const config: UserConfigExport = {
         javascriptEnabled: true,
         // antd 定制主题样式
         modifyVars: {
-          '@fill-body': '#fff',
-        },
-      },
+          '@fill-body': '#fff'
+        }
+      }
     },
     modules: {
-      localsConvention: 'camelCase',
-    },
+      localsConvention: 'camelCase'
+    }
   },
   build: {
     target: 'es2015',
@@ -63,35 +53,32 @@ const config: UserConfigExport = {
     cssCodeSplit: true,
     polyfillDynamicImport: true,
     rollupOptions: {
-      plugins: [],
-    },
-  },
-}
+      plugins: []
+    }
+  }
+};
 
 export default ({ command, mode }: ConfigEnv) => {
   // 官方策略顺序
-  const envFiles = [
-    /** mode file */ `.env.${mode}`,
-    /** default file */ `.env`,
-  ]
-  const { plugins = [], build = {} } = config
+  const envFiles = [/** mode file */ `.env.${mode}`, /** default file */ `.env`];
+  const { plugins = [], build = {} } = config;
 
   for (const file of envFiles) {
     try {
-      fs.accessSync(file, fs.constants.F_OK)
-      const envConfig = dotenv.parse(fs.readFileSync(file))
+      fs.accessSync(file, fs.constants.F_OK);
+      const envConfig = dotenv.parse(fs.readFileSync(file));
       for (const k in envConfig) {
         if (Object.prototype.hasOwnProperty.call(envConfig, k)) {
-          process.env[k] = envConfig[k]
+          process.env[k] = envConfig[k];
         }
       }
     } catch (error) {
-      console.log('配置文件不存在，忽略')
+      console.log('配置文件不存在，忽略');
     }
   }
 
-  const isBuild = command === 'build'
-  config.base = process.env.VITE_STATIC_CDN
+  const isBuild = command === 'build';
+  config.base = process.env.VITE_STATIC_CDN;
 
   if (isBuild) {
     // 压缩 Html 插件
@@ -101,12 +88,12 @@ export default ({ command, mode }: ConfigEnv) => {
       visualizer({
         open: true,
         gzipSize: true,
-        brotliSize: true,
-      }),
-    ]
+        brotliSize: true
+      })
+    ];
     config.define = {
-      'process.env.NODE_ENV': '"production"',
-    }
+      'process.env.NODE_ENV': '"production"'
+    };
   }
 
   // 开发服务器的代理
@@ -117,10 +104,10 @@ export default ({ command, mode }: ConfigEnv) => {
         '/api': {
           target: process.env.VITE_API_HOST,
           changeOrigin: true,
-          rewrite: (path: any) => path.replace(/^\/api/, ''),
-        },
-      },
-    }
+          rewrite: (path: any) => path.replace(/^\/api/, '')
+        }
+      }
+    };
   }
-  return config
-}
+  return config;
+};
